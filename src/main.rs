3224726b91173b201c51afca507c21d597b156fa
Meek Msaki <meek10x@gmail.com>
2026-02-12 13:09:38 -0600

    chore: remove timestamp from tracing log output

--- src/main.rs
+++ src/main.rs
@@ -27,6 +27,7 @@ impl LspArgs {
     pub async fn run(self) -> Result<()> {
         let sub = tracing_subscriber::fmt()
             .compact()
+            .without_time()
             .with_file(true)
             .with_line_number(true)
             .with_thread_ids(true)
